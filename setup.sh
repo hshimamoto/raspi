1530636589bbfe5065c7a6e02b2aba617e8f9454
Hiroshi Shimamoto <hshimamoto@users.noreply.github.com>
2020-12-20 17:11:34 +0900

    support 2020-12-02 images

--- setup.sh
+++ setup.sh
@@ -67,7 +67,11 @@ if [ -x $RPI_TemplateDir/precheck.sh ]; then
 	fi
 fi
 
-RPIOS=2020-08-20-raspios-buster-$RPI_Arch$RPI_Base
+if [ "$RPI_Arch" == "armhf" ]; then
+	RPIOS=2020-12-02-raspios-buster-$RPI_Arch$RPI_Base
+else
+	RPIOS=2020-08-20-raspios-buster-$RPI_Arch$RPI_Base
+fi
 RPIOS_ZIP=images/$RPIOS.zip
 RPIOS_IMG=$RPIOS.img
 
