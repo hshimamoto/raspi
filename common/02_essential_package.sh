bdefd5bd4c8127d2fb5509875102650f8f84484b
Hiroshi Shimamoto <hshimamoto@users.noreply.github.com>
2021-01-01 08:32:33 +0900

    retry in installing packages

--- common/02_essential_package.sh
+++ common/02_essential_package.sh
@@ -3,5 +3,8 @@
 # non interactive
 export DEBIAN_FRONTEND=noninteractive
 
-# install essential packages
-apt-get install -y vim screen git
+exit_code=1
+while [ $exit_code -ne 0 ]; do
+	apt-get install -y vim screen git
+	exit_code=$?
+done
