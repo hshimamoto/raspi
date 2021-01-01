66d28a8c1ec426bebe99c8454f62542e259bc66f
Hiroshi Shimamoto <hshimamoto@users.noreply.github.com>
2021-01-01 09:34:50 +0900

    raspi_setup.sh: refactor cleanup

--- raspi_setup.sh
+++ raspi_setup.sh
@@ -13,18 +13,24 @@ MOUNTPOINT=mnt
 
 LOOP=$(losetup -Pf ${IMG} --show)
 trap cleanup EXIT
-function cleanup {
+function cleanup1 {
 	echo 'cleanup 1'
 	losetup -d ${LOOP}
+}
+function cleanup {
+	cleanup1
 }
 
 mount ${LOOP}p2 $MOUNTPOINT/
 mount ${LOOP}p1 $MOUNTPOINT/boot
-function cleanup {
+function cleanup2 {
 	echo 'cleanup 2'
 	umount $MOUNTPOINT/boot
 	umount $MOUNTPOINT
-	losetup -d ${LOOP}
+}
+function cleanup {
+	cleanup2
+	cleanup1
 }
 
 mount -o bind,ro {,$MOUNTPOINT}/etc/resolv.conf
@@ -32,22 +38,26 @@ mount --bind {,$MOUNTPOINT}/dev
 mount --bind {,$MOUNTPOINT}/dev/pts
 mount --bind {,$MOUNTPOINT}/sys
 mount --bind {,$MOUNTPOINT}/proc
-function cleanup {
+function cleanup3 {
 	echo 'cleanup 3'
 	umount $MOUNTPOINT/{proc,sys,dev/pts,dev,etc/resolv.conf}
-	umount $MOUNTPOINT/boot
-	umount $MOUNTPOINT
-	losetup -d ${LOOP}
+}
+function cleanup {
+	cleanup3
+	cleanup2
+	cleanup1
 }
 
 cp -a {,$MOUNTPOINT}/usr/bin/qemu-arm-static
-function cleanup {
+function cleanup4 {
 	echo 'cleanup 4'
 	rm $MOUNTPOINT/usr/bin/qemu-arm-static
-	umount $MOUNTPOINT/{proc,sys,dev/pts,dev,etc/resolv.conf}
-	umount $MOUNTPOINT/boot
-	umount $MOUNTPOINT
-	losetup -d ${LOOP}
+}
+function cleanup {
+	cleanup4
+	cleanup3
+	cleanup2
+	cleanup1
 }
 
 FLAG_LD_SO_PRELOAD=0
@@ -56,104 +66,110 @@ if [ -e $MOUNTPOINT/etc/ld.so.preload ]; then
 	FLAG_LD_SO_PRELOAD=1
 fi
 
-function cleanup {
+function cleanup5 {
 	echo 'cleanup 5'
 	if [ $FLAG_LD_SO_PRELOAD -ne 0 ]; then
 		sed -i 's/#CHROOT //g' $MOUNTPOINT/etc/ld.so.preload
 	fi
-	rm $MOUNTPOINT/usr/bin/qemu-arm-static
-	umount $MOUNTPOINT/{proc,sys,dev/pts,dev,etc/resolv.conf}
-	umount $MOUNTPOINT/boot
-	umount $MOUNTPOINT
-	losetup -d ${LOOP}
+}
+function cleanup {
+	cleanup5
+	cleanup4
+	cleanup3
+	cleanup2
+	cleanup1
 }
 
 mkdir -p $MOUNTPOINT/template $MOUNTPOINT/common $MOUNTPOINT/extra
-function cleanup {
+function cleanup6 {
 	echo 'cleanup 6'
 	rmdir $MOUNTPOINT/template $MOUNTPOINT/common $MOUNTPOINT/extra
-	if [ $FLAG_LD_SO_PRELOAD -ne 0 ]; then
-		sed -i 's/#CHROOT //g' $MOUNTPOINT/etc/ld.so.preload
-	fi
-	rm $MOUNTPOINT/usr/bin/qemu-arm-static
-	umount $MOUNTPOINT/{proc,sys,dev/pts,dev,etc/resolv.conf}
-	umount $MOUNTPOINT/boot
-	umount $MOUNTPOINT
-	losetup -d ${LOOP}
+}
+function cleanup {
+	cleanup6
+	cleanup5
+	cleanup4
+	cleanup3
+	cleanup2
+	cleanup1
 }
 
 mount -o bind,ro ${TEMPLATE} $MOUNTPOINT/template
-function cleanup {
+function cleanup7 {
 	echo 'cleanup 7'
 	umount $MOUNTPOINT/template
-	rmdir $MOUNTPOINT/template $MOUNTPOINT/common $MOUNTPOINT/extra
-	if [ $FLAG_LD_SO_PRELOAD -ne 0 ]; then
-		sed -i 's/#CHROOT //g' $MOUNTPOINT/etc/ld.so.preload
-	fi
-	rm $MOUNTPOINT/usr/bin/qemu-arm-static
-	umount $MOUNTPOINT/{proc,sys,dev/pts,dev,etc/resolv.conf}
-	umount $MOUNTPOINT/boot
-	umount $MOUNTPOINT
-	losetup -d ${LOOP}
+}
+function cleanup {
+	cleanup7
+	cleanup6
+	cleanup5
+	cleanup4
+	cleanup3
+	cleanup2
+	cleanup1
 }
 
 mount -o bind,ro common $MOUNTPOINT/common
-function cleanup {
+function cleanup8 {
 	echo 'cleanup 8'
 	umount $MOUNTPOINT/common
-	umount $MOUNTPOINT/template
-	rmdir $MOUNTPOINT/template $MOUNTPOINT/common $MOUNTPOINT/extra
-	if [ $FLAG_LD_SO_PRELOAD -ne 0 ]; then
-		sed -i 's/#CHROOT //g' $MOUNTPOINT/etc/ld.so.preload
-	fi
-	rm $MOUNTPOINT/usr/bin/qemu-arm-static
-	umount $MOUNTPOINT/{proc,sys,dev/pts,dev,etc/resolv.conf}
-	umount $MOUNTPOINT/boot
-	umount $MOUNTPOINT
-	losetup -d ${LOOP}
+}
+function cleanup {
+	cleanup8
+	cleanup7
+	cleanup6
+	cleanup5
+	cleanup4
+	cleanup3
+	cleanup2
+	cleanup1
 }
 
+function cleanup9 {
+	echo 'no cleanup 9'
+}
 if [ -e $NAME.config ]; then
 	touch $MOUNTPOINT/config
 	mount -o bind,ro $NAME.config $MOUNTPOINT/config
-	function cleanup {
+	function cleanup9 {
 		echo 'cleanup 9'
 		umount $MOUNTPOINT/config
-		rm -f $MOUNTPOINT/config
-		umount $MOUNTPOINT/common
-		umount $MOUNTPOINT/template
-		rmdir $MOUNTPOINT/template $MOUNTPOINT/common $MOUNTPOINT/extra
-		if [ $FLAG_LD_SO_PRELOAD -ne 0 ]; then
-			sed -i 's/#CHROOT //g' $MOUNTPOINT/etc/ld.so.preload
-		fi
-		rm $MOUNTPOINT/usr/bin/qemu-arm-static
-		umount $MOUNTPOINT/{proc,sys,dev/pts,dev,etc/resolv.conf}
-		umount $MOUNTPOINT/boot
-		umount $MOUNTPOINT
-		losetup -d ${LOOP}
 	}
 fi
+function cleanup {
+	cleanup9
+	cleanup8
+	cleanup7
+	cleanup6
+	cleanup5
+	cleanup4
+	cleanup3
+	cleanup2
+	cleanup1
+}
 
+function cleanup10 {
+	echo 'no cleanup 10'
+}
 if [ -d $EXTRA ]; then
 	mount -o bind,ro ${EXTRA} $MOUNTPOINT/extra
-	function cleanup {
+	function cleanup10 {
 		echo 'cleanup 10'
 		umount $MOUNTPOINT/extra
-		umount $MOUNTPOINT/config
-		rm -f $MOUNTPOINT/config
-		umount $MOUNTPOINT/common
-		umount $MOUNTPOINT/template
-		rmdir $MOUNTPOINT/template $MOUNTPOINT/common $MOUNTPOINT/extra
-		if [ $FLAG_LD_SO_PRELOAD -ne 0 ]; then
-			sed -i 's/#CHROOT //g' $MOUNTPOINT/etc/ld.so.preload
-		fi
-		rm $MOUNTPOINT/usr/bin/qemu-arm-static
-		umount $MOUNTPOINT/{proc,sys,dev/pts,dev,etc/resolv.conf}
-		umount $MOUNTPOINT/boot
-		umount $MOUNTPOINT
-		losetup -d ${LOOP}
 	}
 fi
+function cleanup {
+	cleanup10
+	cleanup9
+	cleanup8
+	cleanup7
+	cleanup6
+	cleanup5
+	cleanup4
+	cleanup3
+	cleanup2
+	cleanup1
+}
 
 echo Chrooting
 
