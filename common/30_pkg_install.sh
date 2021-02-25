83486f55c49b2007a40fe9bbbe14d57af38cf446
Hiroshi Shimamoto <hshimamoto@users.noreply.github.com>
2021-02-26 07:05:16 +0900

    common/30_pkg_install.sh: add --no-install-recommends

--- common/30_pkg_install.sh
+++ common/30_pkg_install.sh
@@ -5,7 +5,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 exit_code=1
 while [ $exit_code -ne 0 ]; do
-	apt-get install -y "$@"
+	apt-get install -y --no-install-recommends "$@"
 	exit_code=$?
 done
 
